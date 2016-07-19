6ff424b9a9ae91291bbbd0b04b97d57e15f1d704
Robert Lech <robert.lech123@gmail.com>
2016-07-18 20:55:06 -0400

    Adding the more complex install instructions I needed to set up on Ubuntu 16.04.

---
+++ personal_install.sh
@@ -0,0 +1,37 @@
+# clone repository
+cd /home/robert/code/github
+git clone git://github.com/gambitproject/gambit.git gambit.git
+cd gambit.git
+
+# install packages
+sudo apt-get install autotools-dev
+sudo apt-get install automake
+sudo apt-get install libtool
+###### don't know if necessary ####
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
+sudo apt-get update
+###################################
+mkdir m4
+aclocal
+libtoolize
+automake --add-missing
+autoconf
+
+# Now switch to ./src/python to finish python setup
+cd src/python
+
+# necessary install stuff
+https://bootstrap.pypa.io/ez_setup.py
+https://pip.pypa.io/en/stable/installing/
+pip install cython
+# http://stackoverflow.com/questions/21530577/fatal-error-python-h-no-such-file-or-directory
+sudo apt-get install python-dev
+
+# the actual build
+python setup.py build
+sudo python setup.py install
+
+# run tests
+cd gambit/tests
+sudo apt-get install python-nose
+nosetests
