a65ef9928ca47bcface4771221a7a8dd3852f679
robert-7 <robert.lech123@gmail.com>
2016-09-15 04:07:05 +0000

    gen_tree_simple.py now creates the 'I got an Ace' game and solves it correctly [#12].

--- src/python/gambit/tests/test_games/personal_test_games/gen_tree_simple.py
+++ src/python/gambit/tests/test_games/personal_test_games/gen_tree_simple.py
@@ -11,10 +11,12 @@ def create_tree(args):
     g.players.add(PLAYER_1)
     g.players.add(PLAYER_2)
 
-    # Chance node
+    # Chance node set probability of each chance action
     iset = g.root.append_move(g.players.chance, 2)
     iset.actions[0].label = "A"
+    iset.actions[0].prob  = Fraction(1, 3)
     iset.actions[1].label = "QK"
+    iset.actions[1].prob  = Fraction(2, 3)
 
     # Rose's moves
     iset = g.root.children[0].append_move(g.players[PLAYER_1], 2)
@@ -38,12 +40,16 @@ def create_tree(args):
     iset.actions[0].label = "Yes"
     iset.actions[1].label = "No"
 
-    iset = g.root.children[1].children[0].append_move(g.players[PLAYER_2], 2)
+    # we need to add the next two nodes to existing information sets
+    PLAYER_1_Bets_1_infoset = g.root.children[0].children[0].infoset
+    PLAYER_1_Bets_3_infoset = g.root.children[0].children[1].infoset
+
+    iset = g.root.children[1].children[0].append_move(PLAYER_1_Bets_1_infoset)
     iset.label = "{} has QK and {}".format(PLAYER_1, "Bet 1")
     iset.actions[0].label = "Yes"
     iset.actions[1].label = "No"
 
-    iset = g.root.children[1].children[1].append_move(g.players[PLAYER_2], 2)
+    iset = g.root.children[1].children[1].append_move(PLAYER_1_Bets_3_infoset)
     iset.label = "{} has QK and {}".format(PLAYER_1, "Bet 3")
     iset.actions[0].label = "Yes"
     iset.actions[1].label = "No"
@@ -65,6 +71,9 @@ def create_tree(args):
 
     return g
 
+'''
+Takes the default outcome and multiplies it based on the bet value.
+'''
 def multiply_outcome(g, outcome, multiple):
     new_outcome = g.outcomes.add("Default outcome multiplied by {}".format(multiple))
     new_outcome[0] =  multiple
@@ -76,29 +85,53 @@ Solve the game.
 '''
 def solve_game(args):
 
-    # create solver
-    solver = gambit.nash.ExternalEnumMixedSolver()
+    # choose the solver needed for this game
+    if MIXED_STRATEGIES == True:
+        solver = gambit.nash.ExternalEnumMixedSolver()
+    else:
+        solver = gambit.nash.ExternalEnumPureSolver()
     
     # solve game
     solutions = solver.solve(g)
     return solutions
 
 '''
-Create a solutions directory and save the solutions there.
+Create a solutions directory, if necessary, and save the solutions there.
 '''
 def print_solutions(args):
     
     # create directory and cd in
-    os.mkdir(SOLUTIONS_DIRECTORY)
+    if not os.path.exists(SOLUTIONS_DIRECTORY):
+        os.mkdir(SOLUTIONS_DIRECTORY)
     os.chdir(SOLUTIONS_DIRECTORY)
 
     # create file
-    file_name = "{}-PSP.nfg".format(time.strftime("%Y-%m-%d %H:%M:%S"))
+    file_name = "{}-PSP-Solutions.nfg".format(time.strftime("%Y-%m-%d %H:%M:%S"))
     target_file = open(file_name, 'w')
     
     # print solutions
     for solution in solutions:
         target_file.write("{}\n".format(str(solution)))
+    
+    # go back out
+    os.chdir(PARENT_DIRECTORY)
+
+'''
+Create a solutions directory, if necessary, and save the solutions there.
+'''
+def print_game(args):
+    
+    # create directory and cd in
+    if not os.path.exists(SOLUTIONS_DIRECTORY):
+        os.mkdir(SOLUTIONS_DIRECTORY)
+    os.chdir(SOLUTIONS_DIRECTORY)
+
+    # create file
+    file_name = "{}-PSP-Game.nfg".format(time.strftime("%Y-%m-%d %H:%M:%S"))
+    target_file = open(file_name, 'w')
+    
+    # print solutions
+    target_file.write("{}".format(g.write()))
     
     # go back out
     os.chdir(PARENT_DIRECTORY)
@@ -109,22 +142,23 @@ if __name__ == '__main__':
     PARENT_DIRECTORY = ".."
     SAVED_GAMES_DIRECTORY = "saved"
     SOLUTIONS_DIRECTORY = "Solutions-for-PSP-Games-{}".format(time.strftime("%Y-%m-%d %H:%M:%S"))
+    MIXED_STRATEGIES = True
 
-    # get file name
-    if len(sys.argv) != 4:
-        print("ERROR: Please supply: a title, a name for player 1, a name for player 2")
-        sys.exit(2)
-    else:
-        TITLE = sys.argv[1]
+    # get user info
+    if len(sys.argv) == 1:
+        TITLE    = "I've got an Ace!"
+        PLAYER_1 = "Rose"
+        PLAYER_2 = "Colin"
+    elif len(sys.argv) == 4:
+        TITLE    = sys.argv[1]
         PLAYER_1 = sys.argv[2]
         PLAYER_2 = sys.argv[3]
+    else:    
+        print("Usage: python gen_tree_simple [title player_1_name player_1_name]")
+        sys.exit(2)
 
-
-    # create the tree
+    # create the tree, solve the game, print the solutions to a file, print the game
     g = compute_time_of(1, "Creating Tree", create_tree)
-
-    # solve the game
     solutions = compute_time_of(2, "Solving Game", solve_game)
-
-    # print the solutions to a file
-    compute_time_of(3, "Printing Solutions", print_solutions)  
+    compute_time_of(3, "Printing Solutions", print_solutions) 
+    compute_time_of(4, "Printing Game", print_game) 
