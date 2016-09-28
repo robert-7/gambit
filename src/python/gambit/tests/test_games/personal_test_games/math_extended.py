e79d20d2ba0e4056e6e8ab61204d3f219b78801e
robert-7 <robert.lech123@gmail.com>
2016-09-28 05:47:35 +0000

    Added a check for combinations to catch exceptions earlier on.

--- src/python/gambit/tests/test_games/personal_test_games/math_extended.py
+++ src/python/gambit/tests/test_games/personal_test_games/math_extended.py
@@ -1,6 +1,8 @@
 from math import factorial
 
 def combinations(n,k):
+    if (n < k) or (k < 0):
+	    raise ValueError("Either n < k or k < 0: n = {}, k = {}.".format(n, k))
     numerator=factorial(n)
     denominator=(factorial(k)*factorial(n-k))
     answer=numerator/denominator
