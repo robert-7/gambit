c473581fc37b98384bc2cc7bfc631bc2de97cde7
Robert Lech <robert.lech123@gmail.com>
2016-10-27 20:08:37 +0000

    Updated the personal install. It had many things out of order.

--- personal_install.sh
+++ personal_install.sh
@@ -4,19 +4,17 @@ git clone git://github.com/gambitproject/gambit.git gambit.git
 cd gambit.git
 
 # install packages
-sudo apt-get install autotools-dev
-sudo apt-get install automake
-sudo apt-get install libtool
-###### don't know if necessary ####
-# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
-# sudo apt-get update
-###################################
+sudo apt-get install -y g++
+sudo apt-get install -y autotools-dev
+sudo apt-get install -y automake
+sudo apt-get install -y libtool
+
+# do make our program?
 mkdir m4
 aclocal
 libtoolize
 automake --add-missing
 autoconf
-
 ./configure
 make
 sudo make install
@@ -24,7 +22,8 @@ sudo make install
 # Now switch to ./src/python to finish python setup
 cd src/python
 
-# necessary install stuff
+# necessary install python modules, ensuring you have the most updated python version
+sudo apt-get upgrade -y python
 wget https://bootstrap.pypa.io/ez_setup.py
 sudo python ez_setup.py
 
@@ -32,23 +31,28 @@ sudo python ez_setup.py
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py 
 
-pip install cython
+# install cython module
+sudo pip install cython
 
 # this explained a probably I ran into: http://stackoverflow.com/questions/21530577/fatal-error-python-h-no-such-file-or-directory
 sudo apt-get install python-dev
 
-# needed for compilations
-sudo apt-get install g++
-
 # the actual build
 python setup.py build
 sudo python setup.py install
+
+# optional install to write games as xml format
+sudo pip install deuces
+sudo pip install lxml
 
 # run tests
 cd gambit/tests
 sudo apt-get install python-nose
 nosetests
 
-# optional install to write games as xml format
-sudo pip install lxml
-sudo pip install deuces
+# move downloaded files to ignored folder
+cd ../../../..
+mkdir downloads
+mv ez_setup.py downloads/ez_setup.py
+mv get-pip.py downloads/get-pip.py
+mv setuptools-28.6.1.zip downloads/setuptools-28.6.1.zip
